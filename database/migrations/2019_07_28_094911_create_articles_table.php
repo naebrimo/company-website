7f6ea80ac3201019e6d1d7fd03e76cf77d43da15
naebrimo <jeffreyfenis@gmail.com>
2019-07-29 08:40:28 +0800

    Create 2019_07_28_094911_create_articles_table.php

---
+++ database/migrations/2019_07_28_094911_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('navlink_id');
+            $table->string('template')->nullable();
+            $table->string('title')->nullable();
+            $table->text('body');
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
