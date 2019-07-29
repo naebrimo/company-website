585a3dea680e00ee62fdc922d0f48ebe3c6b7a2b
naebrimo <jeffreyfenis@gmail.com>
2019-07-29 08:41:06 +0800

    Create Carousel.php

---
+++ app/Carousel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Carousel extends Model
+{
+    protected $guarded = [];
+
+    public function page()
+    {
+        return $this->belongsTo('App\Page');
+    }
+}
