9f4f2a93b90d50992cae763f9945e3761cc8f037
naebrimo <jeffreyfenis@gmail.com>
2019-07-29 08:39:14 +0800

    Update header.blade.php

--- resources/views/__includes/header.blade.php
+++ resources/views/__includes/header.blade.php
@@ -1,5 +1,5 @@
 <header class="fixed-top">
-    <nav class="navbar navbar-expand-lg navbar-dark bg-translucent-blue">
+    <nav class="navbar navbar-expand-lg navbar-dark {{ ((count($carousels) > 0) ? 'bg-translucent-blue' : 'bg-primary' ) }}">
         <div class="container">
             <a class="navbar-brand text-uppercase" href="{{ route('index') }}">
                 <img src="{{ asset('favicon.png') }}" width="30" height="30" class="d-inline-block align-top" alt="{{ config('app.name') }}">
@@ -10,15 +10,15 @@
             </button>
             <div id="navbarToggle" class="collapse navbar-collapse">
                 <ul id="navbar-nav" class="navbar-nav ml-auto mt-2 mt-lg-0">
-                    @foreach($links as $link)
+                    @foreach($navlinks as $navlink)
                         <li class="nav-item">
-                            @if(count(explode('#',$link['href'])) == 2)
-                                <a class="nav-link text-capitalize" href="{{ implode('#',[route(explode('#', $link['href'])[0]),explode('#', $link['href'])[1]]) }}" title="{{ strtoupper($link['text']) }}">{{ $link['text'] }}</a>
+                            @if(count(explode('#',$navlink->href)) == 2)
+                                <a class="nav-link text-capitalize" href="{{ implode('#',[route(explode('#', $navlink->href)[0]),explode('#', $navlink->href)[1]]) }}" title="{{ strtoupper($navlink->body) }}">{{ $navlink->body }}</a>
                             @else
-                                @if(Route::has($link['href']))
-                                    <a class="nav-link text-capitalize" href="{{ route($link['href']) }}" title="{{ strtoupper($link['text']) }}">{{ $link['text'] }}</a>
+                                @if(Route::has($navlink->body))
+                                    <a class="nav-link text-capitalize" href="{{ route($navlink->href) }}" title="{{ strtoupper($navlink->body) }}">{{ $navlink->body }}</a>
                                 @else
-                                    <a class="nav-link text-capitalize" href="{{ implode('', ['#', $link['href'],]) }}" title="{{ strtoupper($link['text']) }}">{{ $link['text'] }}</a>
+                                    <a class="nav-link text-capitalize" href="{{ implode('', ['#', $navlink->href,]) }}" title="{{ strtoupper($navlink->body) }}">{{ $navlink->body }}</a>
                                 @endif
                             @endif
                         </li>
