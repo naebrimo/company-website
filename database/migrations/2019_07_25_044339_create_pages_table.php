3a544eaea86b22fdaff633dddb8e17910b07c4b5
naebrimo <jeffreyfenis@gmail.com>
2019-07-29 08:40:41 +0800

    Create 2019_07_25_044339_create_pages_table.php

---
+++ database/migrations/2019_07_25_044339_create_pages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->string('name');
+            $table->boolean('active');
+            $table->boolean('authentication');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
