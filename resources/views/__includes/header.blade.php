b2d5da0ca5b271aee0957ce728e7d05f0d11369e
naebrimo <jeffreyfenis@gmail.com>
2019-08-01 11:01:05 +0800

    Update header.blade.php

--- resources/views/__includes/header.blade.php
+++ resources/views/__includes/header.blade.php
@@ -1,15 +1,15 @@
-<header class="fixed-top">
-    <nav class="navbar navbar-expand-lg navbar-dark {{ ((count($carousels) > 0) ? 'bg-translucent-blue' : 'bg-primary' ) }}">
-        <div class="container">
-            <a class="navbar-brand text-uppercase" href="{{ route('index') }}">
-                <img src="{{ asset('favicon.png') }}" width="30" height="30" class="d-inline-block align-top" alt="{{ config('app.name') }}">
-                {{ config('app.name') }}
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div id="navbarToggle" class="collapse navbar-collapse">
-                <ul id="navbar-nav" class="navbar-nav ml-auto mt-2 mt-lg-0">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+        <a class="navbar-brand text-uppercase" href="{{ route('index') }}">
+            <img src="{{ asset('favicon.png') }}" width="30" height="30" class="d-inline-block align-top" alt="{{ config('app.name') }}">
+            {{ config('app.name') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggle" aria-controls="navbarToggle" aria-expanded="false" aria-label="Toggle navbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="navbarToggle" class="collapse navbar-collapse">
+            @if(isset($navlinks) && count($navlinks) > 0 )
+                <ul id="navbar-nav" class="navbar-nav ml-auto">
                     @foreach($navlinks as $navlink)
                         <li class="nav-item">
                             @if(count(explode('#',$navlink->href)) == 2)
@@ -24,7 +24,31 @@
                         </li>
                     @endforeach
                 </ul>
-            </div>
+            @else
+                @if(Auth::guard('admin')->check())
+                    @if(Route::currentRouteNamed('index.dashboard*'))
+                        <ul class="navbar-nav ml-auto">
+                    @else
+                        <ul class="navbar-nav mr-auto">
+                    @endif
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdownUser" class="nav-link text-capitalize" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                logged in as: {{ Auth::guard('admin')->user()->name }}
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownUser">
+                                @if(Route::currentRouteNamed('index.dashboard*'))
+                                    <a class="dropdown-item text-capitalize" href="{{ route('index') }}" title="{{ __('preview website') }}">{{ __('preview website') }}</a>
+                                @else
+                                    <a class="dropdown-item text-capitalize" href="{{ route('index.dashboard.index') }}" title="{{ __('go to dashboard') }}">{{ __('go to dashboard') }}</a>
+                                @endif
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item text-capitalize" href="{{ route('index.dashboard.logout') }}" title="{{ __('logout') }}">{{ __('logout') }}</a>
+                            </div>
+                        </li>
+                    </ul>
+                @endif
+            @endif
         </div>
-    </nav>
-</header>
+    </div>
+    </div>
+</nav>
