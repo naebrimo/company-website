63ab0565979f9d16feb60380aa3f2f1ff989b83e
naebrimo <jeffreyfenis@gmail.com>
2019-08-01 10:56:36 +0800

    Update IndexController.php

--- app/Http/Controllers/Dashboard/IndexController.php
+++ app/Http/Controllers/Dashboard/IndexController.php
@@ -2,26 +2,20 @@
 
 namespace App\Http\Controllers\Dashboard;
 
+use App\Page;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+
 class IndexController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth:admin');
     }
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
         return view('dashboard/index');
