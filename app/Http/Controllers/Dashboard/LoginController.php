bf58357ba3091e2e3055f015003b601155f29333
naebrimo <jeffreyfenis@gmail.com>
2019-08-01 10:56:41 +0800

    Update LoginController.php

--- app/Http/Controllers/Dashboard/LoginController.php
+++ app/Http/Controllers/Dashboard/LoginController.php
@@ -2,38 +2,75 @@
 
 namespace App\Http\Controllers\Dashboard;
 
+use App\Page;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
     protected $redirectTo = '/dashboard';
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+        $this->middleware('guest:admin')->except('logout');
+        $this->page = Page::where('id', 1)->first();
     }
+
+    public function showLoginForm()
+    {
+        $navlinks = $this->page->navlinks()->get();
+        $carousels = null;
+
+        return view('dashboard/login', compact('navlinks', 'carousels'));
+    }
+    protected function login(Request $request)
+    {
+        if(filter_var($request->usernameOrEmail, FILTER_VALIDATE_EMAIL))
+        {
+            $validateParams = [
+                'usernameOrEmail' => 'required|email',
+                'password' => 'required|string|min:8'
+            ];
+            $credentials = [
+                'email' => $request->usernameOrEmail,
+                'password' => $request->password,
+                'active' => TRUE,
+            ];
+        }
+        else
+        {
+            $validateParams = [
+                'usernameOrEmail' => 'required|string|min:3',
+                'password' => 'required|string|min:8'
+            ];
+            $credentials = [
+                'username' => $request->usernameOrEmail,
+                'password' => $request->password,
+                'active' => TRUE,
+            ];
+        }
+        $this->validate($request, $validateParams);
+
+        $remember = $request->remember;
+        if(Auth::guard('admin')->attempt($credentials, $remember))
+        {
+            return redirect()->intended($this->redirectTo);
+        }
+        return redirect()
+            ->back()
+            ->withInput($request->only('usernameOrEmail', 'remember'))
+            ->withErrors(['Access Denied.']);
+    }
+
+    protected function logout(Request $request)
+    {
+        Auth::guard('admin')->logout();
+        return $this->loggedOut($request) ?: redirect('/');
+    }
+    protected function loggedOut(Request $request)
+    {
+        //
+    }
+
 }
