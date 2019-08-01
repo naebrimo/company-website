8ce938e19e4291078358d4ea29fb2064de51f279
naebrimo <jeffreyfenis@gmail.com>
2019-08-01 11:01:17 +0800

    Update index.blade.php

--- resources/views/__layouts/index.blade.php
+++ resources/views/__layouts/index.blade.php
@@ -5,15 +5,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="application-name" content="{{ strtolower(config('app.name')) }}">
     <title>{{ strtoupper(config('app.name')) }}</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="icon" href="{{ asset('favicon.png') }}">
 </head>
-<body data-spy="scroll" data-target="#navbar-nav" data-offset="0">
+<body data-spy="scroll" data-target="#navbar-nav" data-offset="10">
     <div id="{{ strtolower(config('app.name')) }}">
-        @include('__includes/header')
-        <main>@yield('content')</main>
-        @include('__includes/footer')
+        <header class="fixed-top">
+            @include('__includes/header')
+        </header>
+        <main>
+            @yield('content')
+        </main>
+        <footer>
+            @include('__includes/footer')
+        </footer>
     </div>
     <a id="goTopBtn" href="#{{ strtolower(config('app.name')) }}">go top</a>
     <script src="{{ asset('js/app.js') }}" defer></script>
