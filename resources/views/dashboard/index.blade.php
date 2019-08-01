a8256c8994f49db00f174ac6374d4027054fcb8a
naebrimo <jeffreyfenis@gmail.com>
2019-08-01 11:01:21 +0800

    Update index.blade.php

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,10 +1,25 @@
 @extends('__layouts/index')
 
 @section('content')
-    <aside>
-        @include('__includes/sidenav')
-    </aside>
     <section class="section">
-        <div class="card"></div>
+        <div class="container">
+            <div class="row">
+                <aside class="col-md-2">
+                    @include('__includes/sidenav')
+                </aside>
+                <article class="col-md-10">
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <h2>Aliquip dolore exercitation excepteur sunt velit aliqua nostrud.</h2>
+                        </div>
+                    </div>
+                    <div class="card mb-2">
+                        <div class="card-body">
+                            <h2>Aliquip dolore exercitation excepteur sunt velit aliqua nostrud.</h2>
+                        </div>
+                    </div>
+                </article>
+            </div>
+        </div>
     </section>
 @endsection
