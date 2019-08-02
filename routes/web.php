a7fd0115509065304e037b3d2e2431d78ddc96da
naebrimo <jeffreyfenis@gmail.com>
2019-08-02 16:27:28 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -35,13 +35,16 @@
 /**
  * This is for the website proper views
  */
-
 Route::prefix('/')->group(function(){
     Route::get('/', 'IndexController@index')->name('index');
     Route::get('/our-partners/{partner?}', 'IndexController@ourPartners')->name('index.our-partners');
     Route::get('/who-we-are/{section?}', 'IndexController@whoWeAre')->name('index.who-we-are');
     Route::get('/what-we-do/{services?}', 'IndexController@whatWeDo')->name('index.what-we-do');
     Route::get('/contact-us', 'IndexController@contactUs')->name('index.contact-us');
+
+    #Redirect routes
+    Route::redirect('/login', '/dashboard/login');
+    Route::redirect('/logout', '/dashboard/logout');
 });
 
 /**
@@ -58,15 +61,79 @@ Route::prefix('/career')->group(function(){
  */
 
 Route::prefix('/dashboard')->group(function(){
-    Route::get('/', 'Dashboard\IndexController@index')->name('index.dashboard.index');
+    Route::get('/', 'Dashboard\IndexController@index')->name('index.dashboard');
 
-    Route::get('/pages', 'Dashboard\PageController@index')->name('index.dashboard.pages');
-    Route::get('/carousels', 'Dashboard\CarouselController@index')->name('index.dashboard.carousels');
-    Route::get('/navlinks', 'Dashboard\NavlinkController@index')->name('index.dashboard.navlinks');
-    Route::get('/articles', 'Dashboard\ArticleController@index')->name('index.dashboard.articles');
-    Route::get('/career-postings', 'Dashboard\CareerController@index')->name('index.dashboard.career-postings');
-    Route::get('/corporate-news', 'Dashboard\CorporateNewsController@index')->name('index.dashboard.corporate-news');
+    Route::resource('/pages','Dashboard\PageController', [
+        'names' => [
+            'index' => 'index.dashboard.pages',
+            'store' => 'index.dashboard.pages.store',
+            'create' => 'index.dashboard.pages.create',
+            'show' => 'index.dashboard.pages.show',
+            'update' => 'index.dashboard.pages.update',
+            'destroy' => 'index.dashboard.pages.destroy',
+            'edit' => 'index.dashboard.pages.edit',
+        ]
+    ]);
 
+    Route::resource('/carousels','Dashboard\CarouselController', [
+        'names' => [
+            'index' => 'index.dashboard.carousels',
+            'store' => 'index.dashboard.carousels.store',
+            'create' => 'index.dashboard.carousels.create',
+            'show' => 'index.dashboard.carousels.show',
+            'update' => 'index.dashboard.carousels.update',
+            'destroy' => 'index.dashboard.carousels.destroy',
+            'edit' => 'index.dashboard.carousels.edit',
+        ]
+    ]);
+
+    Route::resource('/navlinks','Dashboard\NavlinkController', [
+        'names' => [
+            'index' => 'index.dashboard.navlinks',
+            'store' => 'index.dashboard.navlinks.store',
+            'create' => 'index.dashboard.navlinks.create',
+            'show' => 'index.dashboard.navlinks.show',
+            'update' => 'index.dashboard.navlinks.update',
+            'destroy' => 'index.dashboard.navlinks.destroy',
+            'edit' => 'index.dashboard.navlinks.edit',
+        ]
+    ]);
+
+    Route::resource('/articles','Dashboard\ArticleController', [
+        'names' => [
+            'index' => 'index.dashboard.articles',
+            'store' => 'index.dashboard.articles.store',
+            'create' => 'index.dashboard.articles.create',
+            'show' => 'index.dashboard.articles.show',
+            'update' => 'index.dashboard.articles.update',
+            'destroy' => 'index.dashboard.articles.destroy',
+            'edit' => 'index.dashboard.articles.edit',
+        ]
+    ]);
+
+    Route::resource('/career-postings','Dashboard\CareerController', [
+        'names' => [
+            'index' => 'index.dashboard.career-postings',
+            'store' => 'index.dashboard.career-postings.store',
+            'create' => 'index.dashboard.career-postings.create',
+            'show' => 'index.dashboard.career-postings.show',
+            'update' => 'index.dashboard.career-postings.update',
+            'destroy' => 'index.dashboard.career-postings.destroy',
+            'edit' => 'index.dashboard.career-postings.edit',
+        ]
+    ]);
+
+    Route::resource('/corporate-news','Dashboard\CorporateNewsController', [
+        'names' => [
+            'index' => 'index.dashboard.corporate-news',
+            'store' => 'index.dashboard.corporate-news.store',
+            'create' => 'index.dashboard.corporate-news.create',
+            'show' => 'index.dashboard.corporate-news.show',
+            'update' => 'index.dashboard.corporate-news.update',
+            'destroy' => 'index.dashboard.corporate-news.destroy',
+            'edit' => 'index.dashboard.corporate-news.edit',
+        ]
+    ]);
 
     Route::get('/login', 'Dashboard\LoginController@showLoginForm')->name('index.dashboard.login');
     Route::get('password/reset', 'Dashboard\ForgotPasswordController@showLinkRequestForm')->name('dashboard.password.request');
