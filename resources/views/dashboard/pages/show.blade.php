600b1bb463835599f2f3d7ee0f389701b65d973c
naebrimo <jeffreyfenis@gmail.com>
2019-08-02 16:27:25 +0800

    Create show.blade.php

---
+++ resources/views/dashboard/pages/show.blade.php
@@ -0,0 +1,20 @@
+@extends('__layouts/dashboard')
+@section('dashboardContent')
+    <div class="card mb-2">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-6">
+                    <h2 class="text-muted text-capitalize">show page</h2>
+                </div>
+            </div>
+            <hr />
+            <div class="row">
+                <div class="col-12">
+                    <div class="alert alert-primary text-capitalize" role="alert">
+                        this is a sample alert message
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
