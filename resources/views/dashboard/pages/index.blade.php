11e942a3f98d74392a68d6930b5ca725895e932d
naebrimo <jeffreyfenis@gmail.com>
2019-08-02 16:27:04 +0800

    Create index.blade.php

---
+++ resources/views/dashboard/pages/index.blade.php
@@ -0,0 +1,128 @@
+@extends('__layouts/dashboard')
+@section('dashboardContent')
+    <div class="card mb-2">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-6"><h2>page</h2></div>
+                <div class="col-6">
+                    <form action="{{ route('index.dashboard.pages.create') }}" method="GET" enctype="multipart/form-data">
+                        <div class="input-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text">new page after</span>
+                            </div>
+                            <select class="custom-select" aria-label="page list">
+                                <option>---</option>
+                                <option>root</option>
+                            </select>
+                            <div class="input-group-append">
+                                <button class="btn btn-primary" type="submit">Create</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <hr />
+            <div class="row">
+                <div class="col-12">
+                    <h3>page tree</h3>
+                    <ul>
+                        <li>
+                            <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">home</a>
+                        </li>
+                        <li>
+                            <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">our partners</a>
+                            <ul>
+                                <li>
+                                    <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">partners</a>
+                                    <ul>
+                                        <li>
+                                            <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">brief introduction</a>
+                                        </li>
+                                        <li>
+                                                <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">brands</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">affliates</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">who we are</a>
+                            <ul>
+                                <li>
+                                    <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">introduction</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">brief history</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">mission</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">vision</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">core values</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">what we do</a>
+                            <ul>
+                                <li>
+                                    <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">merchandising</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">sales and marketing</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">warehouse and logistics</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">careers</a>
+                            <ul>
+                                <li>
+                                    <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">cover text</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">career postings</a>
+                                    <ul>
+                                        <li>
+                                            <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">job details</a>
+                                            <ul>
+                                                <li>
+                                                    <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0" title="click to rearrange, modify or delete">application form</a>
+                                                </li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0">corporate news</a>
+                            <ul>
+                                <li>
+                                    <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0">featured</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0">latest</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0">archives</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="{{ route('index.dashboard.pages.show', 1) }}" class="btn btn-link text-capitalize p-0">contact us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
