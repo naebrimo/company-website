52808ef4ee81d68c2c98942cc8e5bb89681cf3e6
naebrimo <jeffreyfenis@gmail.com>
2019-08-09 14:46:52 +0800

    Update sidenav.blade.php

--- resources/views/__includes/sidenav.blade.php
+++ resources/views/__includes/sidenav.blade.php
@@ -3,22 +3,22 @@
         <a class="nav-link text-capitalize {{ Route::currentRouteNamed('index.dashboard') ? 'active' : null }}" href="{{ route('index.dashboard') }}">home</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link text-capitalize {{ Route::currentRouteNamed('index.dashboard.pages') ? 'active' : null }}" href="{{ route('index.dashboard.pages') }}">pages</a>
+        <a class="nav-link text-capitalize {{ Route::currentRouteNamed('index.dashboard.pages*') ? 'active' : null }}" href="{{ route('index.dashboard.pages') }}">pages</a>
     </li>
     <li class="nav-item text-capitalize">
-        <a class="nav-link text-capitalize {{ Route::currentRouteNamed('index.dashboard.carousels') ? 'active' : null }}" href="{{ route('index.dashboard.carousels') }}">carousels</a>
+        <a class="nav-link text-capitalize {{ Route::currentRouteNamed('index.dashboard.carousels*') ? 'active' : null }}" href="{{ route('index.dashboard.carousels') }}">carousels</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link text-capitalize {{ Route::currentRouteNamed('index.dashboard.navlinks') ? 'active' : null }}" href="{{ route('index.dashboard.navlinks') }}">navlinks</a>
+        <a class="nav-link text-capitalize {{ Route::currentRouteNamed('index.dashboard.navlinks*') ? 'active' : null }}" href="{{ route('index.dashboard.navlinks') }}">navlinks</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link text-capitalize {{ Route::currentRouteNamed('index.dashboard.articles') ? 'active' : null }}" href="{{ route('index.dashboard.articles') }}">articles</a>
+        <a class="nav-link text-capitalize {{ Route::currentRouteNamed('index.dashboard.articles*') ? 'active' : null }}" href="{{ route('index.dashboard.articles') }}">articles</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link text-capitalize {{ Route::currentRouteNamed('index.dashboard.career-postings') ? 'active' : null }}" href="{{ route('index.dashboard.career-postings') }}">career postings</a>
+        <a class="nav-link text-capitalize {{ Route::currentRouteNamed('index.dashboard.career-postings*') ? 'active' : null }}" href="{{ route('index.dashboard.career-postings') }}">career postings</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link text-capitalize {{ Route::currentRouteNamed('index.dashboard.corporate-news') ? 'active' : null }}" href="{{ route('index.dashboard.corporate-news') }}">corporate news</a>
+        <a class="nav-link text-capitalize {{ Route::currentRouteNamed('index.dashboard.corporate-news*') ? 'active' : null }}" href="{{ route('index.dashboard.corporate-news') }}">corporate news</a>
     </li>
 
 </ul>
