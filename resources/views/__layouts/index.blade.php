5f5f62da4beb1f33b58ccc6873f308a087680f6d
naebrimo <jeffreyfenis@gmail.com>
2019-08-14 16:48:59 +0800

    Update index.blade.php

--- resources/views/__layouts/index.blade.php
+++ resources/views/__layouts/index.blade.php
@@ -1,6 +1,36 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <meta name="robots" content="index,follow" />
+    <meta name="keywords" content="your, tags"/>
+
+    <meta name="description" content="150 words"/>
+    <meta name="subject" content="your website's subject">
+    <meta name="copyright"content="{{ strtolower(config('app.name')) }}">
+    <meta name="language" content="ES">
+
+    <meta name="revised" content="Sunday, July 18th, 2010, 5:15 pm" />
+    <meta name="Classification" content="Business">
+    <meta name="author" content="name, email@hotmail.com">
+
+    <meta name="designer" content="">
+    <meta name="reply-to" content="email@hotmail.com">
+    <meta name="owner" content="">
+    <meta name="abstract" content="">
+    <meta name="topic" content="">
+    <meta name="summary" content="">
+    <meta name="url" content="http://www.websiteaddrress.com">
+    <meta name="identifier-URL" content="http://www.websiteaddress.com">
+    <meta name="directory" content="submission">
+    <meta name="category" content="">
+    <meta name="coverage" content="Worldwide">
+    <meta name="distribution" content="Global">
+    <meta name="rating" content="General">
+    <meta name="revisit-after" content="7 days">
+    <meta http-equiv="Expires" content="0">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Cache-Control" content="no-cache">
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
